c620762240a8e7a7eb917575af1fe000de08183b
lynchlynch <hitcas_gy@163.com>
2021-03-13 22:48:55 +0800

    win10

--- temp_1.py
+++ temp_1.py
@@ -1,6 +1,1 @@
-import numpy as np
-
-# result = 280.42+328.83+1171.84+1298.32+79.51+97.54+1784.68+2056.83
-# result = 4254.83+2056.83+688.76+97.54
-result = 280.32+79.51+328.83
-print('result = ' + str(result))
+import 
