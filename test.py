9574df983acc88a2ec5b20acb4cfdbae3a046aa8
lynchlynch <yepei1988@163.com>
2021-04-11 21:01:51 +0800

    mac

--- test.py
+++ test.py
@@ -1,45 +1,40 @@
-import pandas as pd
-import numpy as np
-import os
-import chardet
+# -*- coding: UTF-8 -*-
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_auc_score
 
-# from pyecharts import Geo, Map
+# 加载 sklearn 自带的乳腺癌（分类）数据集
+X, y = load_breast_cancer(return_X_y=True)
 
+# print(X, y)
 
-def load_large_dta(fname):
-    import sys
+# 以指定比例将数据集分为训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    train_size=0.875, test_size=0.125, random_state=188
+)
 
-    reader = pd.read_stata(fname, iterator=True)
-    df = pd.DataFrame()
+# 使用 lr 类，初始化模型
+clf = LogisticRegression(
+    penalty="l2", C=1.0, random_state=None, solver="lbfgs", max_iter=3000,
+    multi_class='ovr', verbose=0,
+)
 
-    try:
-        chunk = reader.get_chunk(100 * 1000)
-        while len(chunk) > 0:
-            df = df.append(chunk, ignore_index=True)
-            chunk = reader.get_chunk(100 * 1000)
-            print('.')
-            sys.stdout.flush()
-    except (StopIteration, KeyboardInterrupt):
-        pass
+# 使用训练数据来学习（拟合），不需要返回值，训练的结果都在对象内部变量中
+clf.fit(X_train, y_train)
 
-    print('\nloaded {} rows'.format(len(df)))
+# 使用测试数据来预测，返回值预测分类数据
+y_pred = clf.predict(X_test)
 
-    return df
+# 打印主要分类指标的文本报告
+print('--- report ---')
+print(classification_report(y_test, y_pred))
 
+# 打印模型的参数
+print('--- params ---')
+print(clf.coef_, clf.intercept_)
 
-def deconde_str(string):
-    """
-    解码 dta文件防止 乱码
-    """
-    # re = string.encode('latin-1').decode('utf-8')
-    print(string.dtypes)
-    print('1')
-    re = string.encode('gb18030').decode('utf-8')
-    return re
-
-
-# example
-df_2002_path = 'D:/pydir/health_care/raw_data/Charles2018/Demographic_Background.dta'
-df_2002 = load_large_dta(df_2002_path)
-print(df_2002.dtypes)
-df_2002.to_csv('D:/pydir/health_care/raw_data/Charles2018/Demographic_Background.csv',index=False,encoding="utf_8_sig")
+# 打印 auc
+print('--- auc ---')
+print(roc_auc_score(y_test, y_pred))
